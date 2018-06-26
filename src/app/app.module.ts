fe74f6fdd9fbbb2e38823d7ef974c4bbc98757ad
Axel Baron <axel.baron@o-agence.fr>
2018-06-26 18:00:22 +0200

    Initial Commit : rooting + translation example

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {InfoComponent} from './info/info.component';
+import {HomeComponent} from './home/home.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        InfoComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
+        RouterModule.forRoot([
+                {
+                    path: '',
+                    component: HomeComponent
+                },
+                {
+                    path: 'info',
+                    component: InfoComponent
+                },
+                {
+                    path: 'news',
+                    component: InfoComponent
+                }
+            ]
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
