966e77efa2c039b8b2a212c3ec8f24fd527887da
Axel Baron <axel.baron@o-agence.fr>
2018-10-05 12:02:44 +0200

    Add SCSS pluggins

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class InfoComponent implements OnInit {
 
