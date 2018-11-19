24b296c35124b8b7ce6e68a474369cb9d04203e1
Axel Baron <axel.baron@o-agence.fr>
2018-11-19 16:29:31 +0100

    Select comics in header

--- src/app/header/reading-tools/reading-tools.component.ts
+++ src/app/header/reading-tools/reading-tools.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StripsService } from '../../strips.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reading-tools',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reading-tools.component.scss']
 })
 export class ReadingToolsComponent implements OnInit {
+  strips;
+  url;
+  private beginningUrl = 'strip';
 
-  constructor() { }
+  constructor(
+    private _strips: StripsService,
+    private _router: Router
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this._strips.getAllStrips().subscribe(result => {
+      this.strips = result;
+      console.log('strip', this.strips);
+    });
   }
 
+  selectOnChange(value) {
+    this.navigate(value);
+  }
+
+  navigate(id) {
+    this.url = [];
+    this.url.push(this.beginningUrl);
+    this.url.push(id);
+    console.log('url', this.url);
+    this._router.navigate(this.url);
+  }
 }
