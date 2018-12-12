924ab297d8b156e94cb658142c636ac40418ae8e
Axel Baron <axel.baron@o-agence.fr>
2018-12-12 16:12:13 +0100

    Test

--- src/app/strips/strips.component.ts
+++ src/app/strips/strips.component.ts
@@ -21,45 +21,63 @@ export class StripsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    /*
-    *  - Get the two first strips
-    *  - On click call a new fonction with startAt
-    *  - Get the next strips
-    *  - Push the next strips to the strips table who already exist.
-    *
-    * */
 
-    // Get limit by service.
+    // Subscribe: Run once and every time limit(in service) change.
     this._strips.limit.subscribe(limit => this.limit = limit);
 
-    // Get orderBy by service.
-    this._strips.orderBy.subscribe(orderBy => this.orderBy = orderBy);
+    // Subscribe: Run one time and every time orderBy(in service) change.
+    this._strips.orderBy.subscribe(orderBy => {
+      this.orderBy = orderBy;
 
-    // Get Total strip for displaying btn
-    this._strips.getTotalStrips();
-    this._strips.totalStrip.subscribe(totalStrip => this.totalStrip = totalStrip);
+      if (this.orderBy === 'asc') {
+        this.position = 0;
+      }
 
-    // Get the firsts Strips
+      this._strips.getStrips(this.limit, this.orderBy).subscribe(result => {
+        this.strips = result;
+        console.log('strips', this.strips);
+        if (this.orderBy === 'desc') {
+          this.position = this.position + this.limit;
+        } else {
+          this.position = this.strips[this.strips.length - 1].order;
+        }
+      });
+    });
+
     this._strips.getStrips(this.limit, this.orderBy).subscribe(result => {
       this.strips = result;
-      this.position = this.strips[this.strips.length - 1].order;
+      console.log('strips', this.strips);
+      if (this.orderBy === 'desc') {
+        this.position = this.position + this.limit;
+      } else {
+        this.position = this.strips[this.strips.length - 1].order;
+      }
     });
+
+    // Get Total strip for the ''show more'' btn
+    this._strips.getTotalStrips();
+    this._strips.totalStrip.subscribe(totalStrip => this.totalStrip = totalStrip);
   }
 
   nextStrips() {
+    console.log('nextStrips');
     this._strips.getNextStrips(this.position, this.orderBy, this.limit).subscribe((result: Strip[]) => {
+      console.log('getNextStrips');
       if (this.orderBy === 'desc') {
+        console.log('desc');
         this.position = this.position - this.limit;
         if (this.position < 1) {
           this.lastStrip = true;
         }
       } else {
+        console.log('asc');
         this.position = this.position + this.limit;
-        if (this.totalStrip >= this.position) {
+        if (this.position >= (this.totalStrip - 1)) {
           this.lastStrip = true;
         }
       }
       this.strips = [...this.strips, ...result];
+      console.log('strip Updated', this.strips);
     });
   }
 }
